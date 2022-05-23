f786af8669f10bc3aa3de1021b7d1a2416881655
M J B E 07 <103116968+JMBE-07@users.noreply.github.com>
2022-05-23 17:17:27 +0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -17,7 +17,7 @@ def fuck():
 	print(fack)
 	print("\033[92;1m [1] FUCK K4US4R  \033[96;1m[BYPASS]		")
 	print("\033[93;1m [2] FUCK RIYAD-RR \033[96;1m[BYPASS]	")
-        print("\033[93;1m [3] FUCK ranjha_fuck \033[96;1m[BYPASS
+        print("\033[93;1m [3] FUCK ranjha_fuck \033[96;1m[BYPASS]")
 	print("")
 	xx= input(" [•] CHOICE : ")
 	if xx in ["1","01"]:
