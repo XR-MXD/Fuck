e27d7b547aa2b98c46e583ce52731b66a0592631
M J B E 07 <103116968+JMBE-07@users.noreply.github.com>
2022-05-23 17:20:30 +0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -17,7 +17,7 @@ def fuck():
 	print(fack)
 	print("\033[92;1m [1] FUCK K4US4R  \033[96;1m[BYPASS]		")
 	print("\033[93;1m [2] FUCK RIYAD-RR \033[96;1m[BYPASS]	")
-        print("\033[93;1m [3] FUCK ranjha_fuck \033[96;1m[BYPASS]")
+        print("\033[93;1m [3] FUCK RANJHA \033[96;1m[BYPASS]")
 	print("")
 	xx= input(" [•] CHOICE : ")
 	if xx in ["1","01"]:
@@ -28,5 +28,5 @@ def fuck():
 		os.system('python Fuck-RR.py')
         if xx in ["3","03"]:
 		time.sleep(1.5)
-                os.system('python Fuck-ranjha_fuck.py')
+                os.system('python Fuck-RANJHA.py')
 fuck()
