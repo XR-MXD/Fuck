be66f68aac4c4b4f6f2fd8dfc31229e27f06a7ea
M J B E 07 <103116968+JMBE-07@users.noreply.github.com>
2022-05-23 23:53:14 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -13,7 +13,7 @@ def fuck():
 	print(fack)
 	print("\033[96;1m [1] FUCK K4US4R  \033[96;1m[BYPASS]		")
 	print("\033[93;1m [2] FUCK RIYAD-RR \033[96;1m[BYPASS]	")
-	print("\033[93;1m [3] FUCK RANJHA \033[96;1m[BYPASS]	")
+	print("\033[93;1m [3] FUCK SHANTO \033[96;1m[BYPASS]	")
 	print("")
 	xx= input(" [•] CHOICE : ")
 	if xx in ["1","01"]:
@@ -24,6 +24,6 @@ def fuck():
 		os.system('python Fuck-RR.py')
 	if xx in ["3","03"]:
 		time.sleep(1.5)
-	os.system('python Fuck-RANJHA.py')
+	os.system('python Fuck-SHANTO.py')
 
 fuck()
