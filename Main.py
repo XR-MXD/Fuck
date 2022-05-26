1d7fd511d553bd41bbb45a86fafe3f0bf0f0a756
MR - DECOMPILE <103116968+MR-MJ-07@users.noreply.github.com>
2022-05-27 02:38:17 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -18,26 +18,26 @@ def fuck():
 	print(" \033[1;97m_________________________________________")
 	print("\033[1;91m [1] FUCK K4US4R  \033[96;5m[BYPASS]		")
 	print("\033[1;92m [2] FUCK RIYAD-RR \033[96;5m[BYPASS]	")
-	print("\033[1;93m [3] FUCK SHANTO \033[96;5m[BYPASS]	")
+	print(" \033[1;93m[3] FUCK SHANTO \033[96;5m[BYPASS]	")
 	print("\033[1;94m [4] FUCK RANJHA \033[96;5m[BYPASS]	")
 	print("\033[1;96m [5] FUCK MAHIN \033[96;5m[BYPASS]	")
 	print("")
 	xx= input(" [•] CHOICE : ")
 	if xx in ["1","01"]:
 		time.sleep(1.5)
-    os.system('python Fuck-K4US4R.py')
+		os.system('python Fuck-K4US4R.py')
 	if xx in ["2","02"]:
 		time.sleep(1.5)
-	os.system('python Fuck-RR.py')
+		os.system('python Fuck-RR.py')
 	if xx in ["3","03"]:
 		time.sleep(1.5)
-    os.system('python Fuck-SHANTO.py')
+	os.system('python Fuck-SHANTO.py')
 	if xx in ["4","04"]:
 		time.sleep(1.5)
-    os.system('python Fuck-RANJHA.py')
+	os.system('python Fuck-RANJHA.py')
 	if xx in ["5","05"]:
 		time.sleep(1.5)
-    os.system('python Fuck-MAHIN.py')
+	os.system('python Fuck-MAHIN.py')
 	
 
 fuck()
