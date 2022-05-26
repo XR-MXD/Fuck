c65b82a84b2e4d214fe2765d7493aaff787eae57
MR - DECOMPILE <103116968+MR-MJ-07@users.noreply.github.com>
2022-05-27 03:00:59 +0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ def fuck():
 \033[1;97m ██ ████ ██ ██████     ██ ████ ██      ██ 
 \033[1;97m ██  ██  ██ ██   ██    ██  ██  ██ ██   ██ 
 \033[1;97m ██      ██ ██   ██ ██ ██      ██  █████
- \033[1;97m_________________________________________
+\033[1;97m _________________________________________
 \033[1;91m [★] FUCK YOUR OK IDZ
 \033[1;92m [★] ABLAMI NOT ALLOW BRO
 \033[1;91m [★] WE ARE NOT SAME BRO
@@ -17,7 +17,7 @@ def fuck():
 	print(" \033[1;97m_________________________________________")
 	print("\033[1;91m [1] FUCK K4US4R  \033[96;5m[BYPASS]		")
 	print("\033[1;92m [2] FUCK RIYAD-RR \033[96;5m[BYPASS]	")
-	print(" \033[1;93m[3] FUCK SHANTO \033[96;5m[BYPASS]	")
+	print("\033[1;93m [3] FUCK SHANTO \033[96;5m[BYPASS]	")
 	print("\033[1;94m [4] FUCK RANJHA \033[96;5m[BYPASS]	")
 	print("\033[1;96m [5] FUCK MAHIN \033[96;5m[BYPASS]	")
 	print("")
@@ -37,7 +37,9 @@ def fuck():
 	if xx in ["3","03"]:
 
 		time.sleep(1.5)
+
 	        os.system('python Fuck-SHANTO.py')
+
 	if xx in ["4","04"]:
 
 		time.sleep(1.5)
